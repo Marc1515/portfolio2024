744506d45c1dea487cc6a0f0fa76736d2550361c
Marc España Quinquilla <meq.1515@gmail.com>
2024-05-21 18:42:53 +0200

    Objects deleted cause the information soesn't come from here anymore and file transformed to .ts

---
+++ app/[locale]/(platform)/_components/Technologies/TechnologiesData.ts
@@ -0,0 +1,34 @@
+import htmlSvgIMG from "./../../assets/img/techSVG/html5-color.svg";
+import cssSvgIMG from "./../../assets/img/techSVG/css3-color.svg";
+import sassSvgIMG from "./../../assets/img/techSVG/sass-color.svg";
+import lessSvgIMG from "./../../assets/img/techSVG/less.svg";
+import javaScriptSvgIMG from "./../../assets/img/techSVG/javascript-color.svg";
+import typeScriptSvgIMG from "./../../assets/img/techSVG/typescript-color.svg";
+import angularSvgIMG from "./../../assets/img/techSVG/angular-color.svg";
+import rxjsSvgIMG from "./../../assets/img/techSVG/reactivex-color.svg";
+import reactSvgIMG from "./../../assets/img/techSVG/react-color.svg";
+import nextSvgIMG from "./../../assets/img/techSVG/nextjs-color.svg";
+import wordpressSvgIMG from "./../../assets/img/techSVG/wordpress-color.svg";
+import wooCommerceSvgIMG from "./../../assets/img/techSVG/woo-color.svg";
+import bootstrapSvgIMG from "./../../assets/img/techSVG/bootstrap-color.svg";
+import tailwindSvgIMG from "./../../assets/img/techSVG/tailwindcss-color.svg";
+
+const TechImages = {
+  techBuild: {
+    htmlSVG: htmlSvgIMG,
+    cssSVG: cssSvgIMG,
+    sassSVG: sassSvgIMG,
+    lessSvg: lessSvgIMG,
+    javaScriptSvg: javaScriptSvgIMG,
+    angularSVG: angularSvgIMG,
+    rxjsSvg: rxjsSvgIMG,
+    typeScriptSvg: typeScriptSvgIMG,
+    reactSVG: reactSvgIMG,
+    nextSVG: nextSvgIMG,
+    wordpressSVG: wordpressSvgIMG,
+    wooCommerceSVG: wooCommerceSvgIMG,
+    bootstrapSVG: bootstrapSvgIMG,
+    tailwindSvg: tailwindSvgIMG,
+  },
+};
+export default TechImages;
