d2bc885fa533686a59306d3da24f39a1dc4f9aa2
Marc España Quinquilla <meq.1515@gmail.com>
2024-06-01 20:57:46 +0200

    Radion buttons finilizad for a small resolucion

--- app/[locale]/(platform)/_components/header/Navbar/LanguageButton/LanguageButton.tsx
+++ app/[locale]/(platform)/_components/header/Navbar/LanguageButton/LanguageButton.tsx
@@ -1,97 +1,123 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter, usePathname } from "next/navigation";
 import { useTranslation } from "react-i18next";
 import i18nConfig from "@/i18nConfig";
 import Image from "next/image";
+import "./LanguageButton.scss";
 
 export const LanguageButton = () => {
   const { i18n } = useTranslation();
   const currentLocale = i18n.language;
   const router = useRouter();
   const currentPathname = usePathname();
+  const [selectedLocale, setSelectedLocale] = useState(currentLocale);
+
+  useEffect(() => {
+    if (selectedLocale !== currentLocale) {
+      const scrollY = window.scrollY;
+      sessionStorage.setItem("scrollPosition", scrollY.toString());
+
+      const hash = window.location.hash;
+
+      const days = 30;
+      const date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      const expires = date.toUTCString();
+      document.cookie = `NEXT_LOCALE=${selectedLocale};expires=${expires};path=/`;
+
+      let newUrl;
+
+      if (
+        currentLocale === i18nConfig.defaultLocale &&
+        !i18nConfig.prefixDefault
+      ) {
+        newUrl = "/" + selectedLocale + currentPathname + hash;
+      } else {
+        newUrl =
+          currentPathname.replace(`/${currentLocale}`, `/${selectedLocale}`) +
+          hash;
+      }
+
+      // Esperar a que la animación CSS termine (500ms)
+      const timer = setTimeout(() => {
+        router.push(newUrl);
+        router.refresh();
+      }, 1000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [currentLocale, currentPathname, router, selectedLocale]);
 
   const handleChange = (newLocale: string) => {
-    // Guardar la posición de desplazamiento actual
-    const scrollY = window.scrollY;
-    sessionStorage.setItem("scrollPosition", scrollY.toString());
-
-    // Obtener el hash actual de la URL
-    const hash = window.location.hash;
-
-    const days = 30;
-    const date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    const expires = date.toUTCString();
-    document.cookie = `NEXT_LOCALE=${newLocale};expires=${expires};path=/`;
-
-    let newUrl;
-
-    if (
-      currentLocale === i18nConfig.defaultLocale &&
-      !i18nConfig.prefixDefault
-    ) {
-      newUrl = "/" + newLocale + currentPathname + hash;
-    } else {
-      newUrl =
-        currentPathname.replace(`/${currentLocale}`, `/${newLocale}`) + hash;
-    }
-
-    router.push(newUrl);
-    router.refresh();
+    setSelectedLocale(newLocale);
   };
 
   return (
-    <div className="w-100 mb-10 md:mb-0 flex justify-center items-center gap-10">
-      <label className="h-20">
-        <input
-          type="radio"
-          name="language"
-          value="en"
-          checked={currentLocale === "en"}
-          onChange={() => handleChange("en")}
-          className="size-7 appearance-none"
-        />
-        <Image
-          src="/flags/england-flag.png"
-          alt="English"
-          width={25}
-          height={25}
-        />
-      </label>
-      <label className="h-20">
-        <input
-          type="radio"
-          name="language"
-          value="es"
-          checked={currentLocale === "es"}
-          onChange={() => handleChange("es")}
-          className="size-7 appearance-none"
-        />
-        <Image
-          src="/flags/spain-flag.png"
-          alt="Spanish"
-          width={25}
-          height={25}
-        />
-      </label>
-      <label className="h-20">
-        <input
-          type="radio"
-          name="language"
-          value="ca"
-          checked={currentLocale === "ca"}
-          onChange={() => handleChange("ca")}
-          className="size-7 appearance-none"
-        />
-        <Image
-          src="/flags/catalonia-flag.png"
-          alt="Catalan"
-          width={25}
-          height={25}
-        />
-      </label>
+    <div className="container">
+      <div className="selector">
+        <div className="selector-item">
+          <input
+            type="radio"
+            id="radio1"
+            name="selector"
+            value="en"
+            checked={selectedLocale === "en"}
+            onChange={() => handleChange("en")}
+            className="selector-item_radio"
+          />
+          <label htmlFor="radio1" className="selector-item_label">
+            <Image
+              src="/flags/england-flag.svg"
+              alt="English"
+              width={25}
+              height={25}
+              className="selector-item_label"
+            />
+          </label>
+        </div>
+        <div className="selector-item">
+          <input
+            type="radio"
+            id="radio2"
+            name="selector"
+            value="es"
+            checked={selectedLocale === "es"}
+            onChange={() => handleChange("es")}
+            className="selector-item_radio"
+          />
+          <label htmlFor="radio2" className="selector-item_label">
+            <Image
+              src="/flags/spain-flag.svg"
+              alt="Spanish"
+              width={25}
+              height={25}
+              className="selector-item_label"
+            />
+          </label>
+        </div>
+        <div className="selector-item">
+          <input
+            type="radio"
+            id="radio3"
+            name="selector"
+            value="ca"
+            checked={selectedLocale === "ca"}
+            onChange={() => handleChange("ca")}
+            className="selector-item_radio"
+          />
+          <label htmlFor="radio3" className="selector-item_label">
+            <Image
+              src="/flags/catalonia-flag.svg"
+              alt="Catalan"
+              width={25}
+              height={25}
+              className="selector-item_label"
+            />
+          </label>
+        </div>
+      </div>
     </div>
   );
 };
