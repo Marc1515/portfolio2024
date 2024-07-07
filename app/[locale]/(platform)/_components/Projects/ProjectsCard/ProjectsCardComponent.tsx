b522c14d0c066807cd1f27e692447145d4e5bb0c
Marc España Quinquilla <meq.1515@gmail.com>
2024-07-07 10:11:33 +0200

    I decided to make the project card ass a component

---
+++ app/[locale]/(platform)/_components/Projects/ProjectsCard/ProjectsCardComponent.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import { AiFillGithub, AiOutlineLink } from "react-icons/ai";
+import { StaticImageData } from "next/image";
+import SwitchButtonComponent from "../SwitchButton/SwitchButtonComponent";
+import { useSwitchButton } from "../../../context/SwitchButtonContext";
+import "./ProjectsCardComponent.scss";
+
+type ProjectsCardProps = {
+  item: {
+    id: number;
+    img: StaticImageData;
+    nombre: string;
+    githubURL: string;
+    pageURL: string;
+  };
+};
+
+export const ProjectsCardComponent = ({ item }: ProjectsCardProps) => {
+  const { flippedIds } = useSwitchButton();
+  const isFlipped = flippedIds.has(item.id);
+
+  return (
+    <div className={`projectsCards__Card ${isFlipped ? "flipped" : ""}`}>
+      <div className="projectsCards_ImageWrapper">
+        <Image src={item.img} alt={item.nombre} />
+      </div>
+      <div className="inner">
+        <div className="projectsCards__FirstSheet">
+          <SwitchButtonComponent id={item.id} />
+        </div>
+
+        <div className="projectsCards__SecondSheet">
+          <div className="projectsCards__NameContainer">
+            <span className="projectsCards__Name">{item.nombre}</span>
+          </div>
+          <div className="projectsCards__Links">
+            <a href={item.githubURL} target="_blank" rel="noreferrer">
+              <AiFillGithub />
+            </a>
+            <a href={item.pageURL} target="_blank" rel="noreferrer">
+              <AiOutlineLink />
+            </a>
+          </div>
+        </div>
+        <div className="projectsCards__ThirdSheet"></div>
+      </div>
+    </div>
+  );
+};
