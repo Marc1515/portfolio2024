a2c24dc86f57ffea6ca4a7109473a91046d45fba
Marc España Quinquilla <meq.1515@gmail.com>
2024-07-07 10:14:13 +0200

    The switch button I've created as a component to turn the card

---
+++ app/[locale]/(platform)/_components/Projects/SwitchButton/SwitchButtonComponent.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { TbSwitchHorizontal } from "react-icons/tb";
+import React from "react";
+import { useSwitchButton } from "../../../context/SwitchButtonContext";
+
+type SwitchButtonProps = {
+  id: number;
+};
+
+const SwitchButtonComponent = ({ id }: SwitchButtonProps) => {
+  const { toggleFlip } = useSwitchButton();
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    toggleFlip(id);
+  };
+
+  return (
+    <Button onClick={handleClick}>
+      <TbSwitchHorizontal />
+    </Button>
+  );
+};
+
+export default SwitchButtonComponent;
