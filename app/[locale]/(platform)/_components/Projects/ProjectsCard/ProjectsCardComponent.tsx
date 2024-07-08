262670c33385c57071b93e3cf08f3b215403b8eb
Marc España Quinquilla <meq.1515@gmail.com>
2024-07-08 18:37:08 +0200

    finaly I did a new motion

--- app/[locale]/(platform)/_components/Projects/ProjectsCard/ProjectsCardComponent.tsx
+++ app/[locale]/(platform)/_components/Projects/ProjectsCard/ProjectsCardComponent.tsx
@@ -4,7 +4,6 @@ import { StaticImageData } from "next/image";
 import SwitchButtonComponent from "../SwitchButton/SwitchButtonComponent";
 import { useSwitchButton } from "../../../context/SwitchButtonContext";
 import "./ProjectsCardComponent.scss";
-import { useEffect, useState } from "react";
 
 type ProjectsCardProps = {
   item: {
@@ -19,38 +18,19 @@ type ProjectsCardProps = {
 export const ProjectsCardComponent = ({ item }: ProjectsCardProps) => {
   const { flippedIds } = useSwitchButton();
   const isFlipped = flippedIds.has(item.id);
-  const [isAnimating, setIsAnimating] = useState(false);
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  useEffect(() => {
-    if (isFlipped) {
-      setIsAnimating(true);
-      setIsExpanded(false);
-
-      const timer = setTimeout(() => {
-        setIsAnimating(false);
-        setIsExpanded(true);
-      }, 1000);
-
-      return () => clearTimeout(timer);
-    }
-  }, [isFlipped]);
 
   return (
-    <div className={`projectsCards__Card ${isFlipped ? "flipped" : ""}`}>
+    <div className={`projectsCards__Card ${isFlipped ? "expanded" : ""}`}>
       <div className="projectsCards_ImageWrapper">
         <Image src={item.img} alt={item.nombre} />
       </div>
       <div className="inner">
         <div className="projectsCards__FirstSheet">
+          <div className="projectCards__SecondName">{item.nombre}</div>
           <SwitchButtonComponent id={item.id} />
         </div>
 
-        <div
-          className={`projectsCards__SecondSheet ${
-            isAnimating ? "fadeOut" : ""
-          } ${isExpanded ? "fadeOut expanded" : ""}`}
-        >
+        <div className="projectsCards__SecondSheet">
           <div className="projectsCards__NameContainer">
             <span className="projectsCards__Name">{item.nombre}</span>
           </div>
