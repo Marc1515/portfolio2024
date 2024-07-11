7626009a253ecd047a7d07c1ccb0d819abc6e6a8
Marc1515 <meq.1515@gmail.com>
2024-07-11 20:12:49 +0200

    I've added the tech's images on the project card to be seen when you clik the button

--- app/[locale]/(platform)/_components/Projects/ProjectsCard/ProjectsCardComponent.tsx
+++ app/[locale]/(platform)/_components/Projects/ProjectsCard/ProjectsCardComponent.tsx
@@ -15,6 +15,7 @@ type ProjectsCardProps = {
     nombre: string;
     githubURL: string;
     pageURL: string;
+    techBuild: any;
   };
 };
 
@@ -51,7 +52,31 @@ export const ProjectsCardComponent = ({ item }: ProjectsCardProps) => {
             </a>
           </div>
         </div>
-        <div className="projectsCards__ThirdSheet"></div>
+        <div className="projectsCards__ThirdSheet">
+          <div className="projectsCards__TechImg">
+            {item.techBuild.htmlSVG && (
+              <Image src={item.techBuild.htmlSVG} alt="HTML" />
+            )}
+            {item.techBuild.cssSVG && (
+              <Image src={item.techBuild.cssSVG} alt="CSS" />
+            )}
+            {item.techBuild.javaScriptSvg && (
+              <Image src={item.techBuild.javaScriptSvg} alt="JavaScript" />
+            )}
+            {item.techBuild.typeScriptSvg && (
+              <Image src={item.techBuild.typeScriptSvg} alt="TypeScript" />
+            )}
+            {item.techBuild.angularSVG && (
+              <Image src={item.techBuild.angularSVG} alt="Angular" />
+            )}
+            {item.techBuild.reactSVG && (
+              <Image src={item.techBuild.reactSVG} alt="React" />
+            )}
+            {item.techBuild.nextSVG && (
+              <Image src={item.techBuild.nextSVG} alt="Next.js" />
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
